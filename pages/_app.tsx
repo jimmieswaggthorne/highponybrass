c8d138fe4e93400c0aa6dc1ef2da46984bc89f3b
Jimmie Swaggthorne <Jimmie.Swaggthorne@nerdery.com>
2024-01-13 12:27:23 -0600

    updated favi

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,15 +15,15 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Jimmie Swaggthorne Template</title>
+        <title>High Pony Brass</title>
         <meta name="description" content="Chicago Based Interdisciplinary performance artist specializing in Burlesque" />
         <link rel="manifest" crossOrigin="use-credentials" href="/manifest.webmanifest" />
         <meta content="/social.png" name="og:image" />
         <meta name="theme-color" content="#000000" />
         <meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1.0, maximum-scale=1" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
-        <link rel="apple-touch-icon" href="/favicon.ico"></link>
-        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/favicon.png"></link>
+        <link rel="icon" type="image/png" href="/favicon.png" />
       </Head>
       <Component {...pageProps} />
     </>
