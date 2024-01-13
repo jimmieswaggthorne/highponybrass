edfd07be24865fd991a5864ad03c6975497c2126
Jimmie Swaggthorne <Jimmie.Swaggthorne@nerdery.com>
2024-01-13 13:09:28 -0600

    updated socials

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,7 +16,7 @@ function MyApp({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>High Pony Brass by Whimsy Stiff-Richards</title>
-        <meta name="description" content="High Pony Brass by Whimsy Stiff-Richards! Dates to be announced soon" />
+        <meta name="description" content="High Pony Brass by Whimsy Stiff-Richards! Dates to be announced soon. Follow @highponybrass for updates" />
         <link rel="manifest" crossOrigin="use-credentials" href="/manifest.webmanifest" />
         <meta content="/social.jpg" name="og:image" />
         <meta name="theme-color" content="#FF70AC" />
