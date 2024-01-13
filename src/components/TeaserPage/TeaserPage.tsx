a2089f24fc959c08d9945a159bf53d4e8a2ebeb6
Jimmie Swaggthorne <Jimmie.Swaggthorne@nerdery.com>
2024-01-13 12:49:08 -0600

    updated logos

--- src/components/TeaserPage/TeaserPage.tsx
+++ src/components/TeaserPage/TeaserPage.tsx
@@ -3,6 +3,8 @@ import styles from './TeaserPage.module.scss'
 const TeaserPage = () => {
   return (
     <div className={styles.wrap}>
+      <img src="/images/whimsy.png" className={styles.whimsy} alt="Whimsy Stiff-Richards" />
+      <img src="/images/whimsy.png" className={`${styles.whimsy} ${styles.right}`} alt="Whimsy Stiff-Richards" />
       <div className={styles.component}>
         <img src="/images/logo-long.png" alt="High Pony Brass Logo" className={styles.logo} />
       </div>
