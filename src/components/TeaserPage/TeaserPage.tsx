bade0d2f207c2f6ccbe8cc59f6846c8fa4078552
Jimmie Swaggthorne <Jimmie.Swaggthorne@nerdery.com>
2024-01-14 11:18:35 -0600

    relocated link

--- src/components/TeaserPage/TeaserPage.tsx
+++ src/components/TeaserPage/TeaserPage.tsx
@@ -4,14 +4,14 @@ import styles from './TeaserPage.module.scss'
 const TeaserPage = () => {
   return (
     <div className={styles.wrap}>
-      <a href="https://instagram.com/highponybrass" target="_blank" className='icon-button'>
-        <InstagramIcon />
-      </a>
       <img src="/images/whimsy.png" className={styles.whimsy} alt="Whimsy Stiff-Richards" />
       <img src="/images/whimsy.png" className={`${styles.whimsy} ${styles.right}`} alt="Whimsy Stiff-Richards" />
       <div className={styles.component}>
         <img src="/images/logo-long.png" alt="High Pony Brass Logo" className={styles.logo} />
       </div>
+      <a href="https://instagram.com/highponybrass" target="_blank" className='icon-button'>
+        <InstagramIcon />
+      </a>
     </div>
   )
 }
