c471e1ffd2c2b036025a67ec96564952a0b9acd1
Jimmie Swaggthorne <Jimmie.Swaggthorne@nerdery.com>
2024-01-14 15:05:37 -0600

    updated bottom icons

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,11 +1,16 @@
-import { InstagramIcon } from "@/assets/Icons"
+import { InstagramIcon, EmailIcon } from "@/assets/Icons"
 import styles from './Footer.module.scss'
 
 const Footer = () => {
   return (
     <footer className={styles.footer}>
+      <a href="mailto:highponybrass@whimsystiff.com">
+        <EmailIcon />
+        <span>highponybrass@whimsystiff.com</span>
+      </a>
       <a href="https://instagram.com/highponybrass" target="_blank" className='icon-button'>
         <InstagramIcon />
+        <span>@highponybrass</span>
       </a>
     </footer>
   )
