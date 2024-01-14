c511336de0e1026d8da78115bdc4b09dbca527c1
Jimmie Swaggthorne <Jimmie.Swaggthorne@nerdery.com>
2024-01-14 14:32:59 -0600

    updates to site

--- src/components/TeaserPage/TeaserPage.tsx
+++ src/components/TeaserPage/TeaserPage.tsx
@@ -7,11 +7,11 @@ const TeaserPage = () => {
       <video src="/video/sparkle-bg.mp4"
         autoPlay className={styles.bgVideo} loop controls={false} muted={true} poster="/images/bg.png">
       </video>
-      <img src="/images/whimsy.png" className={styles.whimsy} alt="Whimsy Stiff-Richards" />
-      <img src="/images/whimsy.png" className={`${styles.whimsy} ${styles.right}`} alt="Whimsy Stiff-Richards" />
       <div className={styles.component}>
         <img src="/images/logo-long.png" alt="High Pony Brass Logo" className={styles.logo} />
       </div>
+      <img src="/images/whimsy.png" className={styles.whimsy} alt="Whimsy Stiff-Richards" />
+      <img src="/images/whimsy.png" className={`${styles.whimsy} ${styles.right}`} alt="Whimsy Stiff-Richards" />
     </div>
   )
 }
