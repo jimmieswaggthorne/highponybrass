ffe753c8e99c1ac7648668a0639ea978ac2e7d4f
jimmieswaggthorne <112135907+jimmieswaggthorne@users.noreply.github.com>
2024-03-14 18:00:36 -0500

    Update HomePage.tsx

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -22,10 +22,10 @@ const HomePage = () => {
             High Pony Brass is a high energy, high camp, and high production cover concert celebrating the music of the Pop & R&B princess herself, Ariana Grande.
           </p>
           <p>
-            Led by Berklee College of Music graduate and Drag Personality Whimsy Stiff-Richards, High Pony Brass takes you on a journey through your favorite high-pony heroine's repetoire with a unique twist; In place of Ari's signature whistle tones are the musing melodies of over 9 different instruments played by Whimsy themself!
+            Led by Berklee College of Music graduate and Drag Personality Whimsy Stiff-Richards, High Pony Brass takes you on a journey through your favorite high-pony heroine's repertoire with a unique twist; In place of Ari's signature whistle tones are the musing melodies of over 9 different instruments played by Whimsy themself!
           </p>
           <p>
-            What would a concert built for a pop princess be without an ensemble? Enjoy backup dancing and unique interactive opportunities with Whimsy's signature Bunnys! Don't forget our featured fan favorites on Ari's hot tracks like Iggy Azalea and Nathan Sykes! Enjoy guest collaborators playing their own instruments!
+            What would a concert built for a pop princess be without an ensemble? Enjoy backup dancing and unique interactive opportunities with Whimsy's signature Bunnies! Don't forget our featured fan favorites on Ari's hot tracks like Iggy Azalea and Nathan Sykes! Enjoy guest collaborators playing their own instruments!
           </p>
           <button className={styles.featuredButton} onClick={featuredClickHandler}>View our featured artists</button>
         </div>
