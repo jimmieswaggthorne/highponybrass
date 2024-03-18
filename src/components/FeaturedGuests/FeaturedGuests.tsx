c9e8f64fa43bc5adc191254fc8dcce393d4e1112
Jimmie Swaggthorne <jimmieswaggthorne@gmail.com>
2024-03-18 12:45:16 -0500

    put all features in

--- src/components/FeaturedGuests/FeaturedGuests.tsx
+++ src/components/FeaturedGuests/FeaturedGuests.tsx
@@ -3,24 +3,48 @@ import styles from './FeaturedGuests.module.scss'
 const FeaturedGuests = () => {
   return (
     <>
+      <div className={styles.kenzie}>
+        <div className={styles.rowBody}>
+          <h1>Featured Artist - Kenzie Coulee (she/her)</h1>
+          <p>
+            Kenzie Coulee is a multimedia artist and musician based in Chicago, combining fashion design, violin and makeup artistry to create the drag persona “Kenzie Couleé”. Kenzie is the first drag daughter of RuPaul's Drag Race contestant Shea Coulee. She studied fashion at the School of the Art Institute of Chicago, and has been part of the drag community of Chicago for 8 years.
+          </p>
+        </div>
+      </div>
       <div className={styles.mandyn}>
         <div className={styles.rowBody}>
           <h1>Featured Artist - Mandyn E. Mueller (he/they)</h1>
           <p>
             Mueller is so excited to be performing with High Pony Brass.
-          </p>
-          <p>
+            <br />
             Born and raised in Michigan, he has been an award-winning dancer, performer, and artist his whole life. He graduated from Saginaw Valley State University with a major in theatre and minor in fine art and moved to Chicago in 2013. Since then, he has been involved as a performer in a variety of shows including The Fly Honey Show, Enter The Garden Asheville (North Carolina), the Toy Soldier and ensemble dancer in The Buttcracker: A Nutcracker Burlesque, and as a featured tap dancer and variety performer in multiple Show Pole! shows at the Newport Theater. He had the pleasure of assistant stage managing for Floor Show at the Chopin Theater in Chicago and most recently joined the cast of Comedy Dance Chicago.
-          </p>
-          <p>See more of Mandyn on their Instagram @mandyn_ellis</p>
+            <br />
+            See more of Mandyn on their Instagram @mandyn_ellis</p>
 
         </div>
-      </div>
+      </div >
       <div className={styles.graham}>
         <div className={styles.rowBody}>
           <h1>Featured Artist - Graham Hawley (he/him)</h1>
           <p>
             Graham Hawley is ecstatic to be returning to the stage for an all-Ariana Grande show! Graham is a multidisciplinary performer and artist residing in Chicago, IL. He got his start in musical theatre and originally moved to Chicago to pursue musical theatre performance, but after getting involved with nightlife, he knew he wanted to pivot his career and began gogo and backup dancing on the North Halsted strip. Since then he has gogo danced throughout the US and backup danced for queens such as CircuitMOM and Denali Foxx. He has also launched a modeling career and worked with brands such as Wayne Underwear and legendary LGBTQ+ photographer Mark Henderson. If you want to follow his journey, you can check him out on Instagram at @grahawley.
+          </p>
+        </div>
+      </div>
+      <div className={styles.thorne}>
+        <div className={styles.rowBody}>
+          <h1>Featured Artist - Thorne (they/them)</h1>
+          <p>
+            Thorne is an interdisciplinary artist and event producer based in Chicago. They have been performing since the age of 13, but producing and performing burlesque around the globe since 2014. Thorne uses high energy acrobatics and musicality to create captivating performances. Thorne is also a co-producer of High Pony Brass and the spouse of the lovely Whimsy Stiff-Richards. See more of Thorne @theonlythorne
+          </p>
+        </div>
+      </div>
+      <div className={styles.david}>
+        <div className={styles.rowBody}>
+          <h1>Featured Artist - David Davis (he/him)</h1>
+          <p>
+            David Davis is a multifaceted performance artist, Dj, choreographer and Baddie. Hailing from Detroit, Mi this electric artist has worked, thrived, created and collabed over the past 9 years here in Chicago creating.... magic. Coming from the BFA Musical Theatre program at Columbia College, David jumped right into equity theatre after college (some credits include Little Theatre on the Square and Paramount Theatre). He then shifted his fierce little gears to the underground queer night scene where he fell in love. After becoming a part of Chicago's Legendary nightlife scene he went on to be apart of a Queer performance group called "The Chanels" where from hosting at Berlin to performing weekly at the Ace hotel to performing at the official GRINDR party and even being featured in the UK's Attitude magazine they definitely made a name for themselves. But he wasn't done yet, he then began his Dj career, moving up the ranks he recently was a featured DJ at Lollapalooza! During this time he never stopped moving his happy feet, his dance career even still flourishes, you can catch him at Pride in the Park yearly and all Circuit MOM events as the head choreographer and a principal dancer, he is thr-thr-thrilled to be joining such a talented cast of different performers in High Pony Brass.
+
           </p>
         </div>
       </div>
