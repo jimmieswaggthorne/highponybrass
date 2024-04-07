212e73cb6fdeaea459d760ecf07f9b6ea9467c5f
Jimmie Swaggthorne <jimmieswaggthorne@gmail.com>
2024-04-07 18:08:34 -0500

    added genito logo to website

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -33,7 +33,7 @@ const HomePage = () => {
       </div>
       <ShowList />
       {/* <FeaturedGuests /> */}
-      {/* <Sponsors /> */}
+      <Sponsors />
     </div>
   )
 }
--- src/components/Sponsors/Sponsors.tsx
+++ src/components/Sponsors/Sponsors.tsx
@@ -5,6 +5,14 @@ const Sponsors = () => {
   return (
     <div className={styles.body}>
       <div className={styles.wrap}>
+        <h2>Our Sponsors</h2>
+        <div className={styles.sponsorWraps}>
+          <div className={styles.logo}>
+            <a href="https://www.genitophoto.com/" target="_blank">
+              <img src="/images/genito.png" />
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   )
