f4e80cc14f2c0d495f700c006b69db2fdb4fbf70
Jimmie Swaggthorne <jimmieswaggthorne@gmail.com>
2024-06-11 21:40:14 -0500

    update site

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,10 @@ import styles from './Header.module.scss'
 const Header = () => {
   return (
     <header className={styles.wrap}>
-      <video src="/video/sparkle-bg.mp4"
+      {/* <video src="/video/sparkle-bg.mp4"
+        autoPlay className={styles.bgVideo} loop controls={false} muted={true} poster="/images/bg.png">
+      </video> */}
+      <video src="/video/promo-reel.mp4"
         autoPlay className={styles.bgVideo} loop controls={false} muted={true} poster="/images/bg.png">
       </video>
       <a href="/" className={styles.component}>
--- src/components/TeaserPage/TeaserPage.tsx
+++ src/components/TeaserPage/TeaserPage.tsx
@@ -1,15 +1,22 @@
 import { InstagramIcon } from '@/assets/Icons'
 import styles from './TeaserPage.module.scss'
+import { useState } from 'react'
 
 const TeaserPage = () => {
+  const [ended, setEnded] = useState(false)
   return (
     <div className={styles.wrap}>
-      <video src="/video/sparkle-bg.mp4"
-        autoPlay className={styles.bgVideo} loop controls={false} muted={true} poster="/images/bg.png">
-      </video>
+      {!ended ? <video src="/video/promo-video.mp4"
+        autoPlay className={styles.bgVideoPromo} controls={false} muted={true} poster="/images/bg.png" onEnded={() => { setEnded(true) }}>
+      </video> :
+        <video src="/video/sparkle-bg.mp4"
+          autoPlay className={styles.bgVideo} loop controls={false} muted={true} poster="/images/bg.png">
+        </video>
+      }
       <div className={styles.component}>
         <img src="/images/full-logo.png" alt="High Pony Brass Logo" className={styles.logo} />
       </div>
+
     </div>
   )
 }
